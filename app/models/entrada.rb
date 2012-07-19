27716499603b08e309842bdabd33a861c9a54a92
Ricardo <marcorcp1@hotmail.com>
2012-07-18 22:53:24 -0500

    Maquetado el Blog (Entradas), y arreglada la descompostura del menú.

--- app/models/entrada.rb
+++ app/models/entrada.rb
@@ -4,6 +4,17 @@ class Entrada < ActiveRecord::Base
   validates_attachment_size :foto, :less_than => 4.megabytes
   validates_presence_of :foto
   
-  has_attached_file :foto, :styles => { :regular => ["615x300#", :jpg], :thumb => ["100x100#", :jpg] },
-                           :convert_options => { :thumb => "-quality 60", :regular => "-quality 80" }
+  has_attached_file :foto, :styles => { :regular => ["150x150#", :jpg], :thumb => ["150x150#", :png] },
+                           :convert_options => { :thumb => Proc.new{self.convert_options}, :regular => Proc.new{self.convert_options} }
+                                                     
+  def self.convert_options
+    trans = ""
+    px = 6
+    trans << " \\( +clone  -threshold -1 "
+    trans << "-draw 'fill black polygon 0,0 0,#{px} #{px},0 fill white circle #{px},#{px} #{px},0' "
+    trans << "\\( +clone -flip \\) -compose Multiply -composite "
+    trans << "\\( +clone -flop \\) -compose Multiply -composite "
+    trans << "\\) +matte -compose CopyOpacity -composite "
+  end
+  
 end
