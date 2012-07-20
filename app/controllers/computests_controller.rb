ed78c5a35f54ab0e214264b80ef3caf04ff735a0
Paul Sutcliffe <paul@kosmyka.com>
2012-07-19 19:03:04 -0500

    Computest finished and tested

--- app/controllers/computests_controller.rb
+++ app/controllers/computests_controller.rb
@@ -1,5 +1,5 @@
 class ComputestsController < InheritedResources::Base
-  #before_filter :authenticate_usuario!
+  before_filter :authenticate_usuario!, :only => [:index, :destroy]
   before_filter :find_pais
   
   def find_pais
@@ -19,46 +19,50 @@ class ComputestsController < InheritedResources::Base
   def create
     @computest = Computest.new(params[:computest])
     if @computest.valid?
-      if params[:peso]
+      if params[:locale]
         @computest.pais = params[:locale]
-
-        bmi = ( params[:peso] ) / ( params[:altura] * params[:altura] )
-  
-        if bmi
-          prime = ( bmi / 25 )
-          @computest.imc = bmi.round
-          prti = prime.to_s.match(/\d+/)
-          ceros = prime.to_s.match(/\.(0+)/)
-
-          if !ceros
-            ceros = []
-            ceros[1] = "0"
-          end
-
-          prcnt = prime.to_s.match(/\.(\d{1,2})/)
-          if prti[0].to_i <= 0
-            @computest.peso_ideal = "Estas #{(100)*(ceros[1].length) - (prcnt[1].to_i)}% debajo de tu peso ideal"
-          else
-            @computest.peso_ideal = "Estas #{prcnt[1].to_i + (100*(prti[0].to_i-1)) }% sobre tu peso ideal"
-          end
-
-          case
-          when bmi < 18.5
-            @computest.estado = "Bajo Peso"
-          when bmi.between?(18.5,25)
-            @computest.estado = "Normal"
-          when bmi.between?(25,30)
-            @computest.estado = "Con Sobrepeso"
-          when bmi.between?(30,35)
-            @computest.estado = "Obeso Clase I"
-          when bmi.between?(35,40)
-            @computest.estado = "Obeso Clase II"
-          when bmi > 40
-            @computest.estado = "Obeso Clase III"
-          end
-        end
       end
       create!(:notice => "Computest guardado correctamente.") 
     end
+  end
+  
+  def show
+    @computest = Computest.find_by_id(params[:id])
+    bmi = ( @computest.peso ) / ( @computest.altura * @computest.altura )
+
+    if bmi
+      prime = ( bmi / 25 )
+      @imc = bmi.round
+      prti = prime.to_s.match(/\d+/)
+      ceros = prime.to_s.match(/\.(0+)/)
+
+      if !ceros
+        ceros = []
+        ceros[1] = "0"
+      end
+
+      prcnt = prime.to_s.match(/\.(\d{1,2})/)
+      if prti[0].to_i <= 0
+        @peso_ideal = "Estas #{(100)*(ceros[1].length) - (prcnt[1].to_i)}% debajo de tu peso ideal"
+      else
+        @peso_ideal = "Estas #{prcnt[1].to_i + (100*(prti[0].to_i-1)) }% sobre tu peso ideal"
+      end
+
+      case
+      when bmi < 18.5
+        @estado = "Bajo Peso"
+      when bmi.between?(18.5,25)
+        @estado = "Normal"
+      when bmi.between?(25,30)
+        @estado = "Con Sobrepeso"
+      when bmi.between?(30,35)
+        @estado = "Obeso Clase I"
+      when bmi.between?(35,40)
+        @estado = "Obeso Clase II"
+      when bmi > 40
+        @estado = "Obeso Clase III"
+      end
+    end
+    @computest.update_attributes(:imc => @imc, :peso_ideal => @peso_ideal, :estado => @estado)
   end
 end
--- app/models/computest.rb
+++ app/models/computest.rb
@@ -1,2 +1,5 @@
 class Computest < ActiveRecord::Base
+  
+  validates :nombre, :apellido, :email, :telefono, :sexo, :altura, :peso, :presence => true
+  
 end
