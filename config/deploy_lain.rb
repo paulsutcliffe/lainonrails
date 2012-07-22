9d6256e9ce453a8c7091a3d3892afc3bb9898a29
Paul Sutcliffe <paul@kosmyka.com>
2012-07-21 20:35:51 -0500

    updating the kosmyka server

---
+++ config/deploy_lain.rb
@@ -0,0 +1,30 @@
+require "bundler/capistrano"
+
+set :gateway, "walterweb@190.41.229.186"
+set :user, "walter"
+set :domain, "192.168.0.2"
+set :project, "lainonrails"
+set :application, "lainonrails"
+set :applicationdir, "~/#{project}"
+set :repository,  "git@github.com:paulsutcliffe/lainonrails.git"
+default_run_options[:pty] = true
+
+ssh_options[:forward_agent] = true
+set :git_enable_submodules, 1
+set :scm, :git
+set :scm_passphrase, ""
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
+set :scm_verbose, true
+set :deploy_to, applicationdir
+
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true        # This is where Rails migrations will run
+
+set :use_sudo, false
+
+after 'deploy:create_symlink' do
+  run "chmod 775 #{applicationdir}/current/public/dispatch.fcgi"
+end
