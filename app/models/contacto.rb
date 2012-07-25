ddd62aad56aa457f821bea4be98192520dbc0886
Ricardo <marcorcp1@hotmail.com>
2012-07-25 01:26:17 -0500

    Changes done in monday?

--- app/models/contacto.rb
+++ app/models/contacto.rb
@@ -1,3 +1,6 @@
 class Contacto < ActiveRecord::Base
   validates :nombre, :apellido, :telefono, :email, :mensaje, :presence => true
+  validates :politicas do |ag|
+    ag.errors.add "Must" unless self.agreement
+  end
 end
