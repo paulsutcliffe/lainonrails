3b0aa74ec5a91ed85a5a9dcadbf19166ced32d53
Paul Sutcliffe <paul@kosmyka.com>
2012-07-27 18:53:53 -0500

    Updated videos for Vimeo

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -4,15 +4,15 @@ class Producto < ActiveRecord::Base
   friendly_id :nombre, :use => :slugged
   
   auto_html_for :video_link1 do
-      youtube(:width => 190, :height => 119)
+      vimeo(:width => 190, :height => 119)
   end
   
   auto_html_for :video_link2 do
-      youtube(:width => 190, :height => 119)
+      vimeo(:width => 190, :height => 119)
   end
   
   auto_html_for :video_link3 do
-      youtube(:width => 190, :height => 119)
+      vimeo(:width => 190, :height => 119)
   end
   
   validates_attachment_content_type :picture, :content_type =>  ['image/png', 'image/jpg', 'image/jpeg']
