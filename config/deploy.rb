5e093f39652e7dc9e04c2bd152edbc022f6e0f65
Paul Sutcliffe <paul@kosmyka.com>
2013-05-06 01:26:18 -0500

    Implemented passenger restart strategy

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,8 +25,13 @@ role :db,  domain, :primary => true        # This is where Rails migrations will
 
 set :use_sudo, false
 
-after 'deploy:create_symlink' do
-  run "chmod 775 #{applicationdir}/current/public/dispatch.fcgi"
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
 end
 
 after "deploy", "refresh_sitemaps"
